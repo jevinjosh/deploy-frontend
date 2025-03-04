f31c6502d83b6e0915764c18f829b2dafcf58c05
jevin josh <jevinjosh000@gmail.com>
2025-03-04 11:15:50 +0530

    update

---
+++ my-app/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>josh</h1>
+      <p>Email: josh.email@example.com</p>
+      <p>Phone: +1234567890</p>
+    </div>
+  );
+}
+
+export default App;
